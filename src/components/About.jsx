6bf3c20ac16ea2b627d35d8376ff60af1b3083d9
Ginny Gu Tang <ginnygu@gmail.com>
2019-09-12 14:31:27 -0400

    added some about me info and react logo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../src/logo.svg'
 
-function About(){
+function About() {
     return (
         <div>
             <h1 className='title'>About Me</h1>
+            <section className='sections'>
+                <img src={logo} className="App-logo"></img>
+                <p>
+                    A Web Developer, with a passion for continuous learning. Through time at General Assembly, Created web applications with Ruby on Rails, React, Javascript, SQL and others. Having been through several different types of industry, I work well with others, becoming highly adaptable and a fast learner. Currently working at Code Immersives to help build minds of future developers, through daily code challenges and providing support during class.
+                </p>
+                <p>
+                    hello
+                </p>
+            </section>
         </div>
     )
 }
