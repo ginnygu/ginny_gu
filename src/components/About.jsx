cd2a2bcc1eb8880ffe355b40f463c93294ab8a1c
Ginny Gu Tang <ginnygu@gmail.com>
2019-11-13 12:53:58 -0500

    changes to height and about me area

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import logo from '../../src/logo.svg'
 function About() {
     return (
         <div>
-            <h1 className='title'>About Me</h1>
+            <h1 className='title about-title'>About Me</h1>
             <section className='sections'>
                 <p className= 'about-me'>
                     I'm a Full-Stack Developer with passion for learning. I enjoy creating new websites for small companies and provide support for those intested in web development. Currently at Code Immersives as a Teacher Assistant, providing guidance in Nodejs, Javascript and Reactjs.
