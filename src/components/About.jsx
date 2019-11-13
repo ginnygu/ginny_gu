7fa49d37498c38540f30403c146d2c96ec50692d
Ginny Gu Tang <ginnygu@gmail.com>
2019-11-13 11:34:07 -0500

    change font and remove some styles;

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,6 @@ function About() {
         <div>
             <h1 className='title'>About Me</h1>
             <section className='sections'>
-                <img src={logo} className="App-logo"></img>
                 <p className= 'about-me'>
                     I'm a Full-Stack Developer with passion for learning. I enjoy creating new websites for small companies and provide support for those intested in web development. Currently at Code Immersives as a Teacher Assistant, providing guidance in Nodejs, Javascript and Reactjs.
                 </p>
