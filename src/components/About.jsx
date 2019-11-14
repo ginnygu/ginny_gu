5f2443cee3efaf92a66c2ec1f48552484ad7f622
Ginny Gu Tang <ginnygu@gmail.com>
2019-11-14 14:47:08 -0500

    added img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../src/logo.svg'
 
 function About() {
     return (
@@ -9,6 +8,10 @@ function About() {
                 <p className= 'about-me'>
                     I'm a Full-Stack Developer with passion for learning. I enjoy creating new websites for small companies and provide support for those intested in web development. Currently at Code Immersives as a Teacher Assistant, providing guidance in Nodejs, Javascript and Reactjs.
                 </p>
+                <p>
+                Also love to travel and explore the world,one place at a time.
+                </p>
+                <img className="travel-photo" src='https://ginsimage.s3.amazonaws.com/IMG_2599.jpg'/>
             </section>
         </div>
     )
